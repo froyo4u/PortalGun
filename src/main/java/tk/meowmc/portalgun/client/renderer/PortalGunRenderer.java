e51489e41320ba85ba4fb2608a5be3a1ed7793a5
MeowMC <70984351+MeowMC@users.noreply.github.com>
2021-04-21 15:45:06 -0400

    fix model not showing up lmao

--- src/main/java/tk/meowmc/portalgun/client/renderer/PortalGunRenderer.java
+++ src/main/java/tk/meowmc/portalgun/client/renderer/PortalGunRenderer.java
@@ -1,5 +1,12 @@
 package tk.meowmc.portalgun.client.renderer;
 
+import com.mojang.blaze3d.systems.RenderSystem;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.render.DiffuseLighting;
+import net.minecraft.client.render.VertexConsumerProvider;
+import net.minecraft.client.render.model.json.ModelTransformation;
+import net.minecraft.client.util.math.MatrixStack;
+import net.minecraft.item.ItemStack;
 import software.bernie.geckolib3.renderer.geo.GeoItemRenderer;
 import tk.meowmc.portalgun.client.renderer.models.PortalGunModel;
 import tk.meowmc.portalgun.items.PortalGunItem;
@@ -7,5 +14,21 @@ import tk.meowmc.portalgun.items.PortalGunItem;
 public class PortalGunRenderer extends GeoItemRenderer<PortalGunItem> {
     public PortalGunRenderer() {
         super(new PortalGunModel());
+    }
+
+    @Override
+    public void render(ItemStack itemStack, ModelTransformation.Mode mode, MatrixStack matrixStackIn, VertexConsumerProvider bufferIn, int combinedLightIn, int combinedOverlayIn) {
+        if (mode == ModelTransformation.Mode.GUI) {
+            RenderSystem.pushMatrix();
+            VertexConsumerProvider.Immediate irendertypebuffer$impl = MinecraftClient.getInstance().getBufferBuilders()
+                    .getEntityVertexConsumers();
+            DiffuseLighting.disableGuiDepthLighting();
+            super.render(itemStack, mode, matrixStackIn, bufferIn, combinedLightIn, combinedOverlayIn);
+            irendertypebuffer$impl.draw();
+            RenderSystem.enableDepthTest();
+            DiffuseLighting.enableGuiDepthLighting();
+            RenderSystem.popMatrix();
+        } else
+            super.render(itemStack, mode, matrixStackIn, bufferIn, combinedLightIn, combinedOverlayIn);
     }
 }
