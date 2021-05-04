5cd2cf15eb426fe18135bf529a512b2d53caa3c0
MeowMC <70984351+MeowMC@users.noreply.github.com>
2021-05-04 13:37:06 -0400

    removed portal mixin and unused class, added mixin to config

--- src/main/java/tk/meowmc/portalgun/misc/CollisionMethods.java
+++
@@ -1,9 +0,0 @@
-package tk.meowmc.portalgun.misc;
-
-
-public class CollisionMethods {
-
-    public static boolean isRecentlyCollidingWithPortal_pg(int age, long collidingPortalActiveTickTime) {
-        return (long)age - collidingPortalActiveTickTime < 5L;
-    }
-}
