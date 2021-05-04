71a39c6f55db4fcc50b292454de0dbd2504cbae2
MeowMC <70984351+MeowMC@users.noreply.github.com>
2021-05-04 14:20:02 -0400

    change to use item cooldown and add custom portal render spawn packet

--- src/main/java/tk/meowmc/portalgun/client/PortalgunClient.java
+++ src/main/java/tk/meowmc/portalgun/client/PortalgunClient.java
@@ -2,13 +2,25 @@ package tk.meowmc.portalgun.client;
 
 import com.qouteall.immersive_portals.McHelper;
 import com.qouteall.immersive_portals.network.McRemoteProcedureCall;
+import com.qouteall.immersive_portals.render.PortalEntityRenderer;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
+import net.fabricmc.fabric.api.client.networking.v1.ClientPlayNetworking;
+import net.fabricmc.fabric.api.client.rendereregistry.v1.EntityRendererRegistry;
+import net.fabricmc.fabric.api.networking.v1.PacketSender;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.network.ClientPlayNetworkHandler;
 import net.minecraft.client.options.KeyBinding;
 import net.minecraft.client.util.InputUtil;
+import net.minecraft.client.world.ClientWorld;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityType;
+import net.minecraft.network.PacketByteBuf;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.Registry;
 import org.lwjgl.glfw.GLFW;
 import software.bernie.geckolib3.renderer.geo.GeoItemRenderer;
 import tk.meowmc.portalgun.Portalgun;
@@ -16,11 +28,39 @@ import tk.meowmc.portalgun.client.renderer.ClawRenderer;
 import tk.meowmc.portalgun.client.renderer.PortalGunRenderer;
 import tk.meowmc.portalgun.misc.RemoteCallables;
 
+import java.util.UUID;
+
+import static tk.meowmc.portalgun.Portalgun.id;
+
 @Environment(EnvType.CLIENT)
 public class PortalgunClient implements ClientModInitializer {
 
-    public static int tickCounter = 3;
-    public static boolean delay = false;
+    public static final Identifier PacketID = id("spawn_packet");
+
+    public static void onEntitySpawn(MinecraftClient client, ClientPlayNetworkHandler handler, PacketByteBuf buf, PacketSender sender) {
+        EntityType<?> type = Registry.ENTITY_TYPE.get(buf.readVarInt());
+        UUID entityUUID = buf.readUuid();
+        int entityID = buf.readVarInt();
+        double x = buf.readDouble();
+        double y = buf.readDouble();
+        double z = buf.readDouble();
+        float pitch = (buf.readByte() * 360) / 256.0F;
+        float yaw = (buf.readByte() * 360) / 256.0F;
+        ClientWorld world = MinecraftClient.getInstance().world;
+        Entity entity = type.create(world);
+        client.execute(() -> {
+            if (entity != null) {
+                entity.updatePosition(x, y, z);
+                entity.updateTrackedPosition(x, y, z);
+                entity.pitch = pitch;
+                entity.yaw = yaw;
+                entity.setEntityId(entityID);
+                entity.setUuid(entityUUID);
+                assert world != null;
+                world.addEntity(entityID, entity);
+            }
+        });
+    }
 
     @Override
     public void onInitializeClient() {
@@ -34,22 +74,19 @@ public class PortalgunClient implements ClientModInitializer {
         });
 
         ClientTickEvents.END_CLIENT_TICK.register(client -> {
-            tickCounter++;
-            if (client.options.keyAttack.isPressed()) {
-                if (tickCounter > 4)
-                    tickCounter = 3;
-                delay = tickCounter % 3 == 0;
-
-                if (delay) {
-                    RemoteCallables.playAnim();
-                    McHelper.executeOnServerThread(() -> {
-                        McRemoteProcedureCall.tellServerToInvoke("tk.meowmc.portalgun.misc.RemoteCallables.portal1Place");
-                    });
-                }
+            if (client.options.keyAttack.isPressed() && !client.player.getItemCooldownManager().isCoolingDown(Portalgun.PORTALGUN)) {
+                RemoteCallables.playAnim();
+                McHelper.executeOnServerThread(() -> {
+                    McRemoteProcedureCall.tellServerToInvoke("tk.meowmc.portalgun.misc.RemoteCallables.portal1Place");
+                });
             }
         });
 
         GeoItemRenderer.registerItemRenderer(Portalgun.PORTALGUN, new PortalGunRenderer());
         GeoItemRenderer.registerItemRenderer(Portalgun.PORTALGUN_CLAW, new ClawRenderer());
+
+        EntityRendererRegistry.INSTANCE.register(Portalgun.CUSTOM_PORTAL, (dispatcher, context) -> new PortalEntityRenderer(dispatcher));
+
+        ClientPlayNetworking.registerGlobalReceiver(PacketID, PortalgunClient::onEntitySpawn);
     }
 }
