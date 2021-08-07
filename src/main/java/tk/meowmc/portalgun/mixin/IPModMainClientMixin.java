b9bfd99b5628ebd3547ef30c88e11e358e93e27c
MeowMC <70984351+MeowMC@users.noreply.github.com>
2021-08-06 22:34:52 -0400

    remove OptiFine warning

---
+++ src/main/java/tk/meowmc/portalgun/mixin/IPModMainClientMixin.java
@@ -0,0 +1,20 @@
+package tk.meowmc.portalgun.mixin;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import qouteall.imm_ptl.core.IPModMainClient;
+
+@Mixin(IPModMainClient.class)
+public class IPModMainClientMixin {
+
+    @Inject(
+            method = "showOptiFineWarning",
+            at = @At("HEAD"),
+            cancellable = true
+    )
+    private static void dontShowOptiFineWarning(CallbackInfo ci) {
+        ci.cancel();
+    }
+}
