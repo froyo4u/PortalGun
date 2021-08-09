b94bf461486a097e6cfd3b61ed6f144f81d9891b
MeowMC <70984351+MeowMC@users.noreply.github.com>
2021-08-08 22:26:19 -0400

    change some stuff

--- src/main/java/tk/meowmc/portalgun/mixin/EntityMixin.java
+++ src/main/java/tk/meowmc/portalgun/mixin/EntityMixin.java
@@ -11,6 +11,7 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import qouteall.imm_ptl.core.portal.Portal;
+import tk.meowmc.portalgun.config.PortalGunConfig;
 import tk.meowmc.portalgun.entities.CustomPortal;
 import tk.meowmc.portalgun.misc.EntityAttatchments;
 import tk.meowmc.portalgun.misc.VelocityTransfer;
@@ -38,35 +39,38 @@ public abstract class EntityMixin implements EntityAttatchments, VelocityTransfe
     @Shadow
     public abstract void setVelocity(Vec3d velocity);
 
-    @Shadow public abstract boolean isSneaking();
+    @Shadow
+    public abstract boolean isSneaking();
 
     @Inject(method = "tick", at = @At("HEAD"), cancellable = true)
     public void tick(CallbackInfo ci) {
-        Vec3d expand = this.getVelocity().multiply(20);
-        Box streachedBB = this.getBoundingBox().stretch(expand);
+        if (PortalGunConfig.get().enabled.portalFunneling) {
+            Vec3d expand = this.getVelocity().multiply(8);
+            Box streachedBB = this.getBoundingBox().stretch(expand);
 
-        List<CustomPortal> globalPortals = this.world.getEntitiesByClass(CustomPortal.class, streachedBB, Portal::isTeleportable);
+            List<CustomPortal> globalPortals = this.world.getEntitiesByClass(CustomPortal.class, streachedBB, Portal::isTeleportable);
 
-        for (CustomPortal globalPortal : globalPortals) {
-            if (streachedBB.intersects(globalPortal.getBoundingBox())) {
-                Vec3d portalFacing = new Vec3d((int) globalPortal.getNormal().getX(), (int) globalPortal.getNormal().getY(), (int) globalPortal.getNormal().getZ());
-                double offsetX = 0;
-                double offsetZ = 0;
-                double offsetY = 0;
+            for (CustomPortal globalPortal : globalPortals) {
+                if (streachedBB.intersects(globalPortal.getBoundingBox())) {
+                    Vec3d portalFacing = new Vec3d((int) globalPortal.getNormal().getX(), (int) globalPortal.getNormal().getY(), (int) globalPortal.getNormal().getZ());
+                    double offsetX = 0;
+                    double offsetZ = 0;
+                    double offsetY = 0;
 
-                Box streachedPortalBB = globalPortal.getBoundingBox().stretch(portalFacing.getX() * Math.abs(this.getVelocity().getX()) * 10, portalFacing.getY() * Math.abs(this.getVelocity().getY()) * 10, portalFacing.getZ() * Math.abs(this.getVelocity().getZ()) * 10);
-                if (streachedPortalBB.intersects(this.getBoundingBox())) {
-                    if (Math.abs(this.getVelocity().y) > Math.abs(this.getVelocity().x) || Math.abs(this.getVelocity().z) > Math.abs(this.getVelocity().x)) {
-                        offsetX = (this.getBoundingBox().getCenter().x - globalPortal.getBoundingBox().getCenter().x) * .05;
+                    Box streachedPortalBB = globalPortal.getBoundingBox().stretch(portalFacing.getX() * Math.abs(this.getVelocity().getX()) * 10, portalFacing.getY() * Math.abs(this.getVelocity().getY()) * 10, portalFacing.getZ() * Math.abs(this.getVelocity().getZ()) * 10);
+                    if (streachedPortalBB.intersects(this.getBoundingBox())) {
+                        if (Math.abs(this.getVelocity().y) > Math.abs(this.getVelocity().x) || Math.abs(this.getVelocity().z) > Math.abs(this.getVelocity().x)) {
+                            offsetX = (this.getBoundingBox().getCenter().x - globalPortal.getBoundingBox().getCenter().x) * .05;
+                        }
+                        if (Math.abs(this.getVelocity().y) > Math.abs(this.getVelocity().z) || Math.abs(this.getVelocity().x) > Math.abs(this.getVelocity().z)) {
+                            offsetZ = (this.getBoundingBox().getCenter().z - globalPortal.getBoundingBox().getCenter().z) * .05;
+                        }
+                        if (Math.abs(this.getVelocity().z) > Math.abs(this.getVelocity().y) || Math.abs(this.getVelocity().x) > Math.abs(this.getVelocity().y)) {
+                            offsetY = (this.getBoundingBox().getCenter().y - globalPortal.getBoundingBox().getCenter().y) * .05;
+                        }
+                        if (!this.getBoundingBox().intersects(globalPortal.getBoundingBox()) && !this.isSneaking())
+                            this.setVelocity(this.getVelocity().add(-offsetX, -offsetY, -offsetZ));
                     }
-                    if (Math.abs(this.getVelocity().y) > Math.abs(this.getVelocity().z) || Math.abs(this.getVelocity().x) > Math.abs(this.getVelocity().z)) {
-                        offsetZ = (this.getBoundingBox().getCenter().z - globalPortal.getBoundingBox().getCenter().z) * .05;
-                    }
-                    if (Math.abs(this.getVelocity().z) > Math.abs(this.getVelocity().y) || Math.abs(this.getVelocity().x) > Math.abs(this.getVelocity().y)) {
-                        offsetY = (this.getBoundingBox().getCenter().y - globalPortal.getBoundingBox().getCenter().y) * .05;
-                    }
-                    if (!this.getBoundingBox().intersects(globalPortal.getBoundingBox()) && !this.isSneaking())
-                        this.setVelocity(this.getVelocity().add(-offsetX, -offsetY, -offsetZ));
                 }
             }
         }
